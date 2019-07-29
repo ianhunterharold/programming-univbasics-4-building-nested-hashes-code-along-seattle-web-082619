899aa97ee57e9518b03092c67218b8039afe4dba
Ian Harold <ianhunterharold@gmail.com>
2019-07-29 18:42:51 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,8 +1,8 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {},
-      :matriarch => {},
+      :patriarch => {:name =>"Lord Montague", :age => "53" },
+      :matriarch => {}
       :hero => {},
       :hero_friends => []
       
